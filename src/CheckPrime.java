e3de76618c254fb198b36771f8e2d724588b3d82
Prashant Rajput <prashant.rajput01@nagarro.com>
2026-05-05 16:11:29 +0530

    learning

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,6 +1,49 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class CheckPrime {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num=234328768;
+        boolean isPrime=checkPrime(234328768,true);
+        System.out.println(num +" is prime "+isPrime);
+
     }
+
+    private static boolean checkPrime(int i, boolean b) {
+        int factors= factors(i,b);
+        return factors==2;
+    }
+
+    private static int factors(Integer num,boolean optimized){
+        LocalDateTime startTime=LocalDateTime.now();
+        int count=0;
+        String approach="";
+        if (optimized) {
+            approach="Optimized";
+            for(int i=1;(i*i)<=num;i++){
+                if(num%i==0){
+                    if (num/i==i) {
+                        count++;
+                    }else{
+                        count++;count++;
+                    }
+
+                }
+            }
+        }else{
+            approach="Non-Optimized";
+            for(int i=1;i<=num;i++){
+                if(num%i==0){
+                    count++;
+                }
+            }
+        }
+
+        LocalDateTime endTime=LocalDateTime.now();
+        long seconds = ChronoUnit.MILLIS.between(startTime, endTime);
+        System.out.println("Time taken is "+seconds+" milli seconds in "+approach+" approach");
+        return count;
+    }
+
 
 }
