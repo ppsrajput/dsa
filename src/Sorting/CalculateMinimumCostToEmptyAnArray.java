ab95a0081c92cbd64858a0372cfeb25932aa908e
Prashant Rajput <rajputprashant742@gmail.com>
2026-08-08 15:17:11 +0530

    dsa

--- src/Sorting/CalculateMinimumCostToEmptyAnArray.java
+++ src/Sorting/CalculateMinimumCostToEmptyAnArray.java
@@ -14,14 +14,15 @@ public class CalculateMinimumCostToEmptyAnArray {
 
     public static void main(String[] args) {
 
-        int[] array={2,1,4};//4,2,1
+        int[] array={2,1,4,6};//4,2,1
         //4,2,1
         //2,1
         //1
 
 
 
-        Arrays.sort(array);System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
         int totalCost=0;
 
         int multiplier = array.length;
@@ -29,7 +30,29 @@ public class CalculateMinimumCostToEmptyAnArray {
             totalCost=totalCost+(array[i]*multiplier);
             multiplier--;
         }
-        System.out.println("Minimum cost to remove all elements is "+totalCost);
+        System.out.println("Minimum cost to remove all elements using sort is "+totalCost);
+
+        /*Doing same with Reverse Sort*/
+
+        int start=0;
+        int end=array.length-1;
+        while (start<end){
+            int temp=array[start];
+            array[start]=array[end];
+            array[end]=temp;
+
+            start++;end--;
+        }
+        System.out.println(Arrays.toString(array));
+
+         multiplier = 1;
+         totalCost=0;
+        for(int i = 0; i< array.length; i++){
+            totalCost=totalCost+(array[i]*multiplier);
+            multiplier++;
+        }
+        System.out.println("Minimum cost to remove all elements using reverse sort is "+totalCost);
+
 
     }
 }
